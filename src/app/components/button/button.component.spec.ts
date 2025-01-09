01cad6fb32a44aa2fb580f646703583fcc1b5f89
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2025-01-09 10:09:19 +0100

    button wip

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -11,7 +11,58 @@ describe('ButtonComponent', () => {
   it('should create component', () => {
     const fixture = TestBed.createComponent(ButtonComponent);
     const component = fixture.componentInstance;
-    
+
     expect(component).toBeTruthy();
+  });
+
+  it('should apply default type "primary"', () => {
+    const fixture = TestBed.createComponent(ButtonComponent);
+    const buttonEl = fixture.nativeElement.querySelector(
+      '[data-testid="button"]'
+    );
+
+    fixture.detectChanges();
+
+    expect(buttonEl.classList).toContain('primary');
+  });
+
+  it('should apply the correct type when type input is set', () => {
+    const fixture = TestBed.createComponent(ButtonComponent);
+    fixture.componentRef.setInput('type', 'danger');
+
+    fixture.detectChanges();
+
+    const buttonEl = fixture.nativeElement.querySelector(
+      '[data-testid="button"]'
+    );
+
+    fixture.detectChanges();
+
+    expect(buttonEl.classList).toContain('danger');
+    expect(buttonEl.classList).not.toContain('primary');
+  });
+
+  it('should set the button as disabled when disabled is true', () => {
+    const fixture = TestBed.createComponent(ButtonComponent);
+    fixture.componentRef.setInput('disabled', true);
+    const button = fixture.nativeElement.querySelector(
+      '[data-testid="button"]'
+    );
+
+    fixture.detectChanges();
+
+    expect(button.hasAttribute('disabled')).toBe(true);
+  });
+
+  it('should not disable the button when disabled is false', () => {
+    const fixture = TestBed.createComponent(ButtonComponent);
+    fixture.componentRef.setInput('disabled', false);
+    const button = fixture.nativeElement.querySelector(
+      '[data-testid="button"]'
+    );
+
+    fixture.detectChanges();
+
+    expect(button.hasAttribute('disabled')).toBe(false);
   });
 });
