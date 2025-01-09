f109e69fd7c94f77fdd8263511ac315995de117f
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2025-01-09 20:51:44 +0100

    app fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, ButtonComponent, InputComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
