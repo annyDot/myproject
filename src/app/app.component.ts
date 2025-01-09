e1628adb42504c39eaad34a6d900f45435282e5d
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2025-01-09 20:52:42 +0100

    removes unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from './components/button/button.component';
-import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-root',
