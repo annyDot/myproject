38e45d642415b190abd1f760e814b57f85090390
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2025-01-13 08:39:23 +0100

    migrate to nx + fix tests

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent],
+      imports: [NavbarComponent, RouterTestingModule],
     }).compileComponents();
   });
 
