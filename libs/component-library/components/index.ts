256a41ce6f1ca3fbb3438c611ebdd050498cc591
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2025-01-13 22:28:08 +0100

    adds comment to index.ts

--- libs/component-library/components/index.ts
+++ libs/component-library/components/index.ts
@@ -1,3 +1,17 @@
+/**
+ * This file exports all components from the library for easier imports.
+ * Instead of importing from specific file paths, components can be imported
+ * directly from '@component-library/components'.
+ *
+ * Example usage:
+ *
+ * ```typescript
+ * import {
+ *   ButtonComponent,
+ * } from '@component-library/components';
+ * ```
+ */
+
 export * from './button/button.component';
 export * from './dropdown/dropdown.component';
 export * from './icon/icon.component';
