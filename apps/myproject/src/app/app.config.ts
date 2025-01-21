87b5a6faeab0aa8a9c443d89a7057d138013cfd7
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2025-01-21 19:10:43 +0100

    adds firebase realtime database + handles env

--- apps/myproject/src/app/app.config.ts
+++ apps/myproject/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { environment } from './environments/environment';
 import { provideKeycloakAngular } from './shared/auth/keycloak.config';
 
 export const appConfig: ApplicationConfig = {
@@ -8,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideKeycloakAngular(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   ],
 };
